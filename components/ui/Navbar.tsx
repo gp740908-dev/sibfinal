486896332f93385cfb8c6b2219306807283c9f33
gp740908-dev <gp740908@gmail.com>
2026-01-16 16:26:21 +0700

    Adjust Navbar image and div height transitions

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -110,8 +110,8 @@ export const Navbar: React.FC<NavbarProps> = ({ onNavigate, currentView }) => {
               alt="StayinUBUD Icon" 
               className={`w-auto object-contain transition-all duration-700 ease-in-out
                 ${isDarkState 
-                  ? 'h-45' // Scrolled Size (Smaller)
-                  : 'h-45 brightness-0 invert' // Top Size (Larger & White)
+                  ? 'h-65' // Scrolled Size (Smaller)
+                  : 'h-65 brightness-0 invert' // Top Size (Larger & White)
                 } 
               `}
             />
@@ -121,7 +121,7 @@ export const Navbar: React.FC<NavbarProps> = ({ onNavigate, currentView }) => {
               className={`flex flex-col items-center justify-center overflow-hidden transition-all duration-700 ease-in-out
                 ${isScrolled 
                   ? 'max-h-0 opacity-0 mt-0' // Hidden state
-                  : 'max-h-[60px] opacity-100 mt-1.5' // Visible state
+                  : 'max-h-[60px] opacity-100 mt-1' // Visible state
                 }
               `}
             >
